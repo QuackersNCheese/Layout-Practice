19bf938b716074c930005ff60c96c339cf8c1def
QuackersNCheese <Quacktastrophe@protonmail.com>
2025-02-22 13:41:17 -0700

    add javascript content load

---
+++ CSS-Float/script.js
@@ -0,0 +1,16 @@
+function goLondon() {
+    document.getElementById("city").innerText = "London";
+    document.getElementById("capital").innerText = "London is the capital city of England. It is the most populous city in the United Kingdom, with a metropolitan area of over 13 million inhabitants.";
+    document.getElementById("blurb").innerText = "Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium.";
+}
+function goTokyo() {
+    document.getElementById("city").innerText = "Tokyo";
+    document.getElementById("capital").innerText = "Tokyo is the capital of Japan. It is the center of the Greater Tokyo Area, and the most populous metropolitan area in the world.";
+    document.getElementById("blurb").innerText = "";
+}
+function goParis() {
+    document.getElementById("city").innerText = "Paris";
+    document.getElementById("capital").innerText = "Paris is the capital of France. The Paris area is one of the largest population centers in Europe, with more than 12 million inhabitants.";
+    document.getElementById("blurb").innerText = "";
+}
+
